1cb02596e2efd4164a42976c410666520e8d8402
Cole McConnell <chm@farmside.co.nz>
2022-08-10 21:44:49 +1200

    implemented hover effect on project items in Selected Works

--- components/landingPage/SelectedWorks.tsx
+++ components/landingPage/SelectedWorks.tsx
@@ -11,13 +11,21 @@ const projectData: SelectedWorks[] = [
         name: "Beludia Chrome Extension",
         techStack: "CSS, HTML, JAVASCRIPT, CHROME API",
     },
+    {
+        name: "Mosa - Twitter Management Dashboard",
+        techStack: "REACT.JS, REDUX, TYPESCRIPT, SCSS, NODE.JS, EXPRESS.JS",
+    },
+    {
+        name: "Beludia Chrome Extension",
+        techStack: "CSS, HTML, JAVASCRIPT, CHROME API",
+    },
 ];
 
 const SelectedWorks = () => {
     return (
         <div className="py-48">
             <h1 className="text-transparent text-10xl text-stroke-md leading-[.75] px-32 pb-6">SELECTED WORK</h1>
-            <div className="mx-32">
+            <div className="mx-32 hover:highlight-top-border">
                 {projectData.map((item, index) => {
                     return <SelectedWorksItem key={index} index={index + 1} data={item} />;
                 })}
--- components/landingPage/SelectedWorksItem.tsx
+++ components/landingPage/SelectedWorksItem.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { ReactElement } from "react";
 import SelectedWorks from "../../models/SelectedWorks";
 
@@ -5,10 +6,10 @@ const SelectedWorksItem: React.FC<{ data: SelectedWorks; index: number }> = prop
     const techStackList = props.data.techStack.split(",").join(" \u2022 ");
 
     return (
-        <div className="flex flex-col justify-between py-6 border-y border-white group hover:border-gray-300 hover:text-gray-300 hover:fill-gray-300">
+        <div className="flex flex-col justify-between py-6 border-t border-white group hover:border-gray-300 hover:text-gray-300 hover:fill-gray-300 highlight-top-border last:hover:border-b">
             <h1 className="text-6xl group-hover:font-gray-300">{props.data.name.toUpperCase()}</h1>
             <div className="flex flex-row items-end">
-                <h2 className="text-9xl text-transparent text-stroke-sm leading-[.70] group-hover:text-stroke-hovered">
+                <h2 className="text-9xl text-transparent text-stroke-sm leading-[.70] group-hover:text-stroke-hovered mr-7">
                     {props.index.toString().padStart(2, "0")}
                 </h2>
                 <p className="font-sans text-base">{techStackList}</p>
